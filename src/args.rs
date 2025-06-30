0ee1ab4c250eda48e6feeb0cc03824f11597c6f0
Ignacy Kajdan <ignacy.kajdan@gmail.com>
2025-06-30 17:10:36 +0200

    Simplify argument parsing logic

--- src/args.rs
+++ src/args.rs
@@ -11,33 +11,18 @@ pub struct Args {
     #[clap(long, value_name = "PATH", help = "Download Agent image")]
     pub da: PathBuf,
 
-    #[clap(
-        long,
-        value_name = "PATH",
-        help = "Optional Firmware Image Package image"
-    )]
+    #[clap(long, value_name = "PATH", help = "Optional Firmware Image Package image")]
     pub fip: Option<PathBuf>,
 
     #[clap(long, value_name = "PATH", help = "Optional system image")]
     pub img: Option<PathBuf>,
 
-    #[clap(
-        long,
-        value_name = "DEVICE",
-        help = "Path to the device (e.g. /dev/ttyACM0)"
-    )]
+    #[clap(long, value_name = "DEVICE", help = "Path to the device (e.g. /dev/ttyACM0)")]
     pub dev: String,
 }
 
 impl Args {
     pub fn parse() -> Self {
-        Self::try_parse().unwrap_or_else(|e| {
-            let msg = e.to_string().replace(
-                "The following required arguments were not provided:",
-                "Missing required arguments:",
-            );
-            eprintln!("{msg}");
-            std::process::exit(1);
-        })
+        <Self as clap::Parser>::parse()
     }
 }
